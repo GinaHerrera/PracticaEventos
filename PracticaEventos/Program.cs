6684bb8aaa968eea079635e73037d4d44b8576ab
GinaHerrera <56738788+GinaHerrera@users.noreply.github.com>
2019-12-26 10:46:12 -0600

    Agregar archivos de proyecto.

---
+++ PracticaEventos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PracticaEventos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeerTexto();
+        }
+
+        public static void LeerTexto()
+        {
+            string[] lineas;
+
+            lineas = File.ReadAllLines(@"C:\Ejercicios\PracticaEventos\archivo.txt");
+
+            char[] delimitador = { ',' };
+
+            DateTime fecha = DateTime.Now;
+          
+
+            Console.WriteLine("Serie de eventos");
+
+            foreach (string line in lineas)
+            {
+               
+                Console.WriteLine("\t" + line);
+            }
+          
+            Console.ReadKey();
+        }
+        
+    }
+}
